e0b8921abb6bd4c8c45ae81d2230c29861361860
steph45607 <staniswinata.stephanie@gmail.com>
2023-01-07 18:15:43 +0700

    fixed rabin karp

--- RabinKarp.py
+++ RabinKarp.py
@@ -1,6 +1,8 @@
 # Rabin-Karp algorithm in python
+import tracemalloc
+import time
 
-
+# change this to how many possible inputs
 d = 10
 
 def search(pattern, text, q):
@@ -13,12 +15,12 @@ def search(pattern, text, q):
     j = 0
 
     for i in range(m-1):
-        h = (h*d) % q
+        h = (h*d) % q 
 
     # Calculate hash value for pattern and text
     for i in range(m):
-        p = (d*p + ord(pattern[i])) % q
-        t = (d*t + ord(text[i])) % q
+        p = (d*p + ord(pattern[i])) % q # hash value for pattern
+        t = (d*t + ord(text[i])) % q # hash value for text
 
     # Find the match
     for i in range(n-m+1):
@@ -29,16 +31,32 @@ def search(pattern, text, q):
 
             j += 1
             if j == m:
-                print("Pattern is found at position: " + str(i+1))
+                return str(i+1)
 
         if i < n-m:
             t = (d*(t-ord(text[i])*h) + ord(text[i+m])) % q
 
             if t < 0:
                 t = t+q
+		
 
-
+# CHANGE HERE, ALSO CHANGE d VALUE ON LINE 6
 text = "ABCCDDAEFG"
-pattern = "CDD"
+pattern = "CDDE"
+# CHANGE THIS TO the next prime number from the length of text
 q = 13
-search(pattern, text, q)
+
+tracemalloc.start()
+start = time.time()
+x = search(pattern, text, q)
+end = time.time()
+memory = tracemalloc.get_traced_memory()
+tracemalloc.stop()
+tracemalloc.clear_traces()
+
+print("time: " + str(end-start))
+print("memory: " + str(memory[1]))
+if x == None:
+	print("Not found")
+else:
+	print("Found at index: " + (x))
